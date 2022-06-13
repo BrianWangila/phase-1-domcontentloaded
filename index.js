e61073478aa9c7464ecc6a502d36b0857d83934a
Brian Wangila <bwanjalah@gmail.com>
2022-06-13 19:49:11 +0300

    commit tree 674e62722572a310968ccad88355fc2d2ff654ea

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const text = document.getElementById("text")
+
+document.addEventListener("DOMContentLoaded", function(){
+  return text.textContent = "This is really cool!"
+})
+
